6f24cfb7c8cf7ae85adcc37100f20c60384a3dc4
Dom Loong <dom.loong@freshbooks.com>
2022-06-29 21:16:30 -0400

    Add pseudocode

--- budget_tracking_tool.py
+++ budget_tracking_tool.py
@@ -1,8 +1,33 @@
+from os import set_inheritable
 import gspread
 import pandas as pd
 
+
 def main():
-    pass
+    import_csv()
+        figure out which csv belongs to which tabs
+
+            split between expenses and income
+
+        return all imports as a dictionary containing the data
+
+
+    update_expenses()
+
+        get all values from each sheet 
+
+        compare timestamp and the csv data where not in sheet  
+
+        combine to make a new dataframe
+
+
+    update_income()
+
+    similar as above but for expenses
+
+
+    Also need to ensure we deal with dollar amount as string and upload back as float
+
 
 if __name__ == "__main__":
-    def main()
+    main()
