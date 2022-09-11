bb79471db1fed7bf9627d01ce72b096f655b1d6d
Dom Loong <dom.loong@freshbooks.com>
2022-09-10 22:46:11 -0400

    Update to using service account

--- panda_playground.py
+++ panda_playground.py
@@ -1,9 +1,9 @@
 import gspread
 import pandas as pd
 
-gc = gspread.oauth()
+gc = gspread.service_account()
 
-sh = gc.open("Budget Tracking Tool - 2022")
+sh = gc.open("Budget Tracking Tool - 2022 - Tester")
 
 result = sh.values_batch_get(["Mastercard Expense!B8:D", "VISA Expense!B8:D", "Income!B8:D"])
 
