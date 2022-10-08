d5185a779e989f6a3f945053c6b04cfe9366b62f
Dom Loong <dom.loong@freshbooks.com>
2022-10-08 09:35:25 -0400

    Pull sample data into dataframe from mastercard statement

--- budget_tracking_tool.py
+++ budget_tracking_tool.py
@@ -1,36 +1,46 @@
-from os import set_inheritable
 import gspread
 import pandas as pd
 
+gc = gspread.service_account()
 
-def main():
-    import_csv()
-        figure out which csv belongs to which tabs
+sh = gc.open("Budget Tracking Tool - 2022 - Tester")
 
-            split between expenses and income
+result = sh.values_batch_get(["Mastercard Expense!B8:D", "VISA Expense!B8:D", "Income!B8:D"])
 
-        return all imports as a dictionary containing the data
+df_sheet = pd.DataFrame(result["valueRanges"][0]["values"])
 
-
-    update_expenses()
-
-        get all values from each sheet 
-
-        compare timestamp and the csv data where not in sheet  
-
-        combine to make a new dataframe
+df = pd.read_csv("report.csv")
+df = df[["Date", "Description", "Amount"]]
 
 
-    update_income()
+# def main():
+#     import_csv()
+#         figure out which csv belongs to which tabs
 
-    similar as above but for expenses
+#             split between expenses and income
 
-
-    Also need to ensure we deal with dollar amount as string and upload back as float
-
-
-    Also think about how to handle yearly change
+#         return all imports as a dictionary containing the data
 
 
-if __name__ == "__main__":
-    main()
+#     update_expenses()
+
+#         get all values from each sheet 
+
+#         compare timestamp and the csv data where not in sheet  
+
+#         combine to make a new dataframe
+
+
+#     update_income()
+
+#     similar as above but for expenses
+
+
+#     Also need to ensure we deal with dollar amount as string and upload back as float
+
+
+#     Also think about how to handle yearly change
+
+
+# if __name__ == "__main__":
+#     main()
