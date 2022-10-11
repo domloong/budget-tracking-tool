7d5738d8aee11e5245b4b1835ad864c1c81f9612
Dom Loong <dom.loong@freshbooks.com>
2022-10-11 16:26:25 -0400

    Ignore .csv files in git and convert datetime columns to actual datetime type

--- budget_tracking_tool.py
+++ budget_tracking_tool.py
@@ -1,6 +1,8 @@
 import gspread
 import pandas as pd
 
+from datetime import datetime
+
 gc = gspread.service_account()
 
 sh = gc.open("Budget Tracking Tool - 2022 - Tester")
@@ -8,11 +10,13 @@ sh = gc.open("Budget Tracking Tool - 2022 - Tester")
 result = sh.values_batch_get(["Mastercard Expense!B8:D", "VISA Expense!B8:D", "Income!B8:D"])
 
 df_sheet = pd.DataFrame(result["valueRanges"][0]["values"])
+df_sheet = df_sheet.drop(df_sheet[df_sheet[0] == ""].index)
+df_sheet[0] = df_sheet[0].apply(lambda x: datetime.strptime(x, "%m-%d-%Y"))
 
 df = pd.read_csv("report.csv")
 df = df[["Date", "Description", "Amount"]]
 
-
+df["Date"] = df["Date"].apply(lambda x: datetime.strptime(x, "%m/%d/%Y"))
 # def main():
 #     import_csv()
 #         figure out which csv belongs to which tabs
