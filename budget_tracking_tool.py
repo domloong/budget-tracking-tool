ae98c9bb31fe1aa0312711b72eb3e2f1be9b93bc
Dom Loong <dom.loong@freshbooks.com>
2022-10-11 22:11:19 -0400

    Rename google sheet columns and concat two dataframes

--- budget_tracking_tool.py
+++ budget_tracking_tool.py
@@ -12,6 +12,7 @@ result = sh.values_batch_get(["Mastercard Expense!B8:D", "VISA Expense!B8:D", "I
 df_sheet = pd.DataFrame(result["valueRanges"][0]["values"])
 df_sheet = df_sheet.drop(df_sheet[df_sheet[0] == ""].index)
 df_sheet[0] = df_sheet[0].apply(lambda x: datetime.strptime(x, "%m-%d-%Y"))
+df_sheet = df_sheet.rename(columns = {0:"Date", 1:"Description", 2:"Amount"})
 
 df = pd.read_csv("report.csv")
 df = df[["Date", "Description", "Amount"]]
@@ -19,6 +20,8 @@ df = df[["Date", "Description", "Amount"]]
 df["Date"] = df["Date"].apply(lambda x: datetime.strptime(x, "%m/%d/%Y"))
 
 df_new_expenses = df[df.Amount < 0 ]
+
+df_new = pd.concat([df_new_expenses, df_sheet])
 
 # def main():
 #     import_csv()
