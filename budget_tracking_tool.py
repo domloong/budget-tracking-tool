69f34de753001c7f9952dabf686e955b95461f5e
Dom Loong <dom.loong@freshbooks.com>
2022-10-15 18:04:01 -0400

    Add more logic to extract and load data into mastercard tab

--- budget_tracking_tool.py
+++ budget_tracking_tool.py
@@ -13,17 +13,29 @@ df_sheet = pd.DataFrame(result["valueRanges"][0]["values"])
 df_sheet = df_sheet.drop(df_sheet[df_sheet[0] == ""].index)
 df_sheet[0] = df_sheet[0].apply(lambda x: datetime.strptime(x, "%m-%d-%Y"))
 df_sheet = df_sheet.rename(columns = {0:"Date", 1:"Description", 2:"Amount"})
+df_sheet.Amount = df_sheet.Amount.apply(lambda x: float(x[1:]))
 
 df = pd.read_csv("report.csv")
 df = df[["Date", "Description", "Amount"]]
 
 df["Date"] = df["Date"].apply(lambda x: datetime.strptime(x, "%m/%d/%Y"))
 
-df_new_expenses = df[df.Amount < 0 ]
+df_new_expenses = df[(df.Amount < 0) & (df.Date > df_sheet.Date.max())]
+df_new_expenses.Amount = df_new_expenses.Amount.apply(abs)
 
 df_new = pd.concat([df_new_expenses, df_sheet])
 
+df_new["Date"] = df_new["Date"].apply(lambda x: datetime.strftime(x, "%m-%d-%Y"))
+
+df_new = df_new.sort_values("Date")
+
+ws = sh.worksheet("Mastercard Expense")
+
+values = df_new.values.tolist()
+
 df_refunds = df[(df.Amount > 0) & (~df.Description.str.contains("Payment"))]
+
+ws.update("B8:D", values)
 
 # def main():
 #     import_csv()
